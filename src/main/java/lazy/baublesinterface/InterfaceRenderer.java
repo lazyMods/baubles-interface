ad94a48a3aa5b87471802c6e232181853978ded3
lazynessmind <lazynessmind@gmail.com>
2021-08-12 18:55:33 +0100

    Working on 1.17

--- src/main/java/lazy/baublesinterface/InterfaceRenderer.java
+++ src/main/java/lazy/baublesinterface/InterfaceRenderer.java
@@ -2,10 +2,7 @@ package lazy.baublesinterface;
 
 import com.mojang.datafixers.util.Pair;
 import lazy.baubles.api.BaublesAPI;
-import lazy.baubles.api.cap.IBaublesItemHandler;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -18,20 +15,20 @@ public class InterfaceRenderer {
     @SubscribeEvent
     public static void onRenderOverlay(RenderGameOverlayEvent event) {
         if (!InterfaceConfigs.ENABLED.get()) return;
-        if (event.getType() == RenderGameOverlayEvent.ElementType.EXPERIENCE) {
+        if (event.getType() == RenderGameOverlayEvent.ElementType.LAYER) {
             renderInterface(event.getWindow().getGuiScaledWidth(), event.getWindow().getGuiScaledHeight());
         }
     }
 
     @OnlyIn(Dist.CLIENT)
     private static void renderInterface(int gsWidth, int gsHeight) {
-        ClientPlayerEntity player = Minecraft.getInstance().player;
+        var player = Minecraft.getInstance().player;
         if (player != null) {
-            IBaublesItemHandler baublesInv = BaublesAPI.getBaublesHandler(player).orElseThrow(NullPointerException::new);
+            var baublesInv = BaublesAPI.getBaublesHandler(player).orElseThrow(NullPointerException::new);
 
             int xOff = 0, yOff = 0;
             int size = 16 * 4;
-            Pair<Integer, Integer> startPos = resolveStartPos(gsWidth, gsHeight, size);
+            var startPos = resolveStartPos(gsWidth, gsHeight, size);
 
             if (InterfaceConfigs.VERTICAL.get()) {
                 yOff = 16;
@@ -40,7 +37,7 @@ public class InterfaceRenderer {
             }
 
             for (int i = 0; i < 4; i++) {
-                ItemStack stackInSlot = baublesInv.getStackInSlot(i);
+                var stackInSlot = baublesInv.getStackInSlot(i);
                 Minecraft.getInstance().getItemRenderer().renderGuiItem(stackInSlot, startPos.getFirst() + xOff * i, startPos.getSecond() + yOff * i);
                 Minecraft.getInstance().getItemRenderer().renderGuiItemDecorations(Minecraft.getInstance().font, stackInSlot, startPos.getFirst() + xOff * i, startPos.getSecond() + yOff * i);
             }
